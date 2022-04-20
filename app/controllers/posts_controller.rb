c80cd84f6888b5f23df4490771046ec55ba4adea
Ryan Leibering <rsleiberin@gmail.com>
2022-04-20 09:20:31 -0700

    migrated server

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+    def create
+    end
+    def index
+    end
+    def destroy
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
     has_many :categories
     has_many :posts
     has_many :comments
+    validation :username, length: {minimum: 3}
+    validation :password, confirmation: {case_sensitive: true}, length: {minimum: 8}
 
 end
